cf83bcdcbccfd007d9c7f6a71cabf39ab0baa38c
sread <sr15adb@herts.ac.uk>
2016-01-14 11:50:18 +0000

    edited where the test fits file was

--- tests/test_table.py
+++ tests/test_table.py
@@ -3,7 +3,7 @@ from astropandas import Table
 
 class TestTable(TestCase):
     def setUp(self):
-        self.t = Table.read('merged.fits')
+        self.t = Table.read('tests/merged.fits')
         self.df = self.t.to_pandas()
 
     def test_to_pandas(self):
